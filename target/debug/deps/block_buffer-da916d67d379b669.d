3d963e0db5da4e801173ed3fa79ea05e3d524a20
Aaron He <AaronHeDev@outlook.com>
2026-04-20 19:06:08 +1000

    update(build): add darwin build options and remove target from git

--- target/debug/deps/block_buffer-da916d67d379b669.d
+++
@@ -1,6 +0,0 @@
-E:\Desktop\6453\group project\sphincs-rs\target\debug\deps\block_buffer-da916d67d379b669.d: C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\block-buffer-0.10.4\src\lib.rs C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\block-buffer-0.10.4\src\sealed.rs
-
-E:\Desktop\6453\group project\sphincs-rs\target\debug\deps\libblock_buffer-da916d67d379b669.rmeta: C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\block-buffer-0.10.4\src\lib.rs C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\block-buffer-0.10.4\src\sealed.rs
-
-C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\block-buffer-0.10.4\src\lib.rs:
-C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\block-buffer-0.10.4\src\sealed.rs:
--- target/debug/deps/cpufeatures-f4e13d641227ce0e.d
+++
@@ -1,6 +0,0 @@
-E:\Desktop\6453\group project\sphincs-rs\target\debug\deps\cpufeatures-f4e13d641227ce0e.d: C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.2.17\src\lib.rs C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.2.17\src\x86.rs
-
-E:\Desktop\6453\group project\sphincs-rs\target\debug\deps\libcpufeatures-f4e13d641227ce0e.rmeta: C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.2.17\src\lib.rs C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.2.17\src\x86.rs
-
-C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.2.17\src\lib.rs:
-C:\Users\Dell\ 5510\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.2.17\src\x86.rs:
